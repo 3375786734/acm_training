7731eab38982c5093bb50312bfd6b2a549f4f92c
li1113 <www.375786734@qq.com>
2018-09-19 22:16:00 +0800

    edit

---
+++ CF/R510_Div2/E_good.cpp
@@ -0,0 +1,22 @@
+/*
+ *题意:给一个矩阵,和一个chip,chip中存放的是一个矩阵中的数,每次都等可能的从中取出一个比当前数小的放到chip中并得到上面的分数,问分数的期望值是多少
+ *
+ *1.求后缀和
+ *2.注意有相同值的元素
+ */
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> PP;
+typedef pair<int,PP> NN;
+const int maxn=;
+int N,M;
+NN nn[maxn]
+int main()
+{
+	scanf("%d%d",&N,&M);
+	rep(i,1,N)
+		rep(j,1,M)scanf("%d",&a[i][j]);
+	scanf("%d%d",&r,&c);
+
+}
