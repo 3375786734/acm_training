14725e9872e2483b28b79e3eddd53161833466e8
li1113 <www.375786734@qq.com>
2019-04-13 10:51:14 +0800

    edit

--- topics/search/HDU2181_DFS_Good.cpp
+++ topics/search/HDU2181_DFS_Good.cpp
@@ -11,39 +11,35 @@ typedef pair<int,int> PP;
 //const int maxn=;
 //const double esp=1e-6
 int cnt ,S,vis[25];
-int nn[25][4];
-stack<int> st;
+int nn[25][4],ans[25];
 void dfs(int now,int dep)
 {
-	if(dep==19&&now == S){
-		printf("%d:  ",++cnt);
-		printf("%d",st.top());st.pop();
-		while(!st.empty()){
-			printf(" %d",st.top());
-			st.pop();
-		}
-		printf("\n");
-		return ;
-	}
-	else if(dep>20)return ;
+	if(dep>21||(now == S && dep!=1))return ;
 	for(int i=0;i<3;i++){
 		int id =nn[now][i];
+		if(dep==20&&id == S){
+			printf("%d:  ",++cnt);
+			printf("%d",S);
+			for(int i=1;i<20;i++)
+			  printf(" %d",ans[i]);
+			printf(" %d\n",S);
+			return ;
+		}
 		if(vis[id])continue;
-		vis[id]= 1;
+		vis[id]= 1;ans[dep] = id;
 		dfs(nn[now][i],dep+1);
 		vis[id] = 0;
 	}
 }
-
 int main()
 {
-	int a;
-	for(int i=0;i<20;i++)
-	  for(int j =0;j<3;j++){
-		scanf("%d",&a);
-		nn[i][j] = a;
-	  }
+	int a[3];
+	for(int i =1;i<=20;i++){
+		scanf("%d%d%d",&a[0],&a[1],&a[2]);
+		sort(a,a+3);
+		for(int j=0;j<3;j++)nn[i][j] = a[j];
+	}
 	scanf("%d",&S);
+	vis[S] = 1; //note that vis[S] should be fixed
 	dfs(S,1);
 }
-
